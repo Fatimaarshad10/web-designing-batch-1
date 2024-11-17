f4030649bbaaaccc9a7adc2e2350e2472fee4e1c
Fatimaarshad10 <fatimaarshad118@gmail.com>
2024-11-17 19:36:31 +0500

    Javascript last class

---
+++ classes/Class no 10/javascript.js
@@ -0,0 +1,141 @@
+// primitive Data Type
+// String
+// Number
+// undefine
+// null
+// Boolean
+
+// Type variable 
+// let , const , var
+// Non primitive & Complex Data type
+// Array 
+// Object 
+
+// arthimetic operator
+// logical operator 
+// assignment operator
+// increment operator 
+// decrement operator 
+
+
+// "fatima2233"
+/* 'fatima22'
+123 */
+
+// variable
+// let , const , var
+// let a ;
+// let b ;
+// let c ;
+
+// a = 10 
+// b = 20 
+
+// const a = 10
+// a = 20 
+
+// var b = 10;
+// b = 20 
+// console.log(b)
+
+
+// let name = 'fatima'
+
+// let a = false
+// let b = true 
+
+
+// let a = null 
+// console.log(a)
+// let a ;
+// console.log(a)
+
+// Operators
+// Add , Sub , Multiplay , Dividion , modulus
+// c = a + b 
+// console.log(c)
+// c = a * b
+// console.log(c)
+// c = a - b 
+// console.log(c)
+// c = b % a
+// console.log(c)
+// c = a / b 
+// console.log(c)
+
+
+// let personArray ;
+// personArray = ['fatima' , 18 , 'fatima@gmail.com' , true]
+// // method
+// console.log(personArray.length)
+// console.log(personArray[0])
+// console.log(personArray[1])
+// console.log(personArray[2])
+// console.log(personArray[3])
+// console.log(personArray[4])
+
+// Object 
+// const person = {
+//     "name":"saira", //property
+//     "age": 26,
+//     "email":"saira@gmail.com",
+//     "quality": "loyal"
+// }
+// console.log(person.age)
+// console.log(person.email)
+// console.log(person.quality)
+
+// console.warn('hello')
+// console.log("hello world")
+// console.error("for error")
+
+// logical operator 
+// && and  both are true --> true 
+// || or both are false ---> false
+// ! not reverse true ---> false 
+
+// let a = 20 
+// a = a + 10
+// a += 10 
+// a-= 10
+// a *= 10 
+// a /= 10 
+// a %=10
+// console.log(a)
+
+
+// const a = 10 
+// const b = 30 
+
+// const checkCondition = ((a === 10) && (b === 20))
+// const checkCondition = ((a === 10) || (b === 20))
+// const checkCondition = (!(a === 10))
+// console.log(checkCondition)
+
+// let a = 10 
+// a-- 
+// a++ 
+
+// conditions 
+// const age = 60
+// if (age === 10) {
+//     console.log('teenager')
+// }
+// else if (age >= 20) {
+//     console.log("Adult")
+// }
+// else {
+//     console.log('Your age is', null)
+// }
+
+// let marks = 75
+
+// if (marks <= 100 && marks >= 80) {
+//     console.log('Grade is A')
+// }
+// else if (marks <= 70 && marks >= 50) {
+//     console.log('Grade is B')
+// }
+// else {
+//     console.log('Grade is C')
+// }
